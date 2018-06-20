00bd16ae8d29fb4ea24dd46c68508705485a3c56
Za Wilgustus <zancas@contains.io>
2018-06-20 13:09:15 -0600

    add scan of f1

--- tests/test_utf8.py
+++ tests/test_utf8.py
@@ -70,43 +70,14 @@ def test_scan_leading_f0_byte_range():
                                          continuation2=191,
                                          continuation3=191)
 
-def test_scan_leading_f0_byte():
-    invalid_continuation_byte = 0
-    total_count = 0
-    other_exception_count = 0
-    decoded_count = 0
-    inval_cont_bytes = {}
-    for c1 in range(MIN_CONTINUATION, MAX_CONTINUATION+1):
-        for c2 in range(MIN_CONTINUATION, MAX_CONTINUATION+1):
-            for c3 in range(MIN_CONTINUATION, MAX_CONTINUATION+1):
-                total_count = total_count + 1
-                try:
-                    temp_fb = FourByte(MIN_LEADING,
-                                       c1, c2, c3)
-                    decoded = bytes(temp_fb).decode("utf-8")
-                    decoded_count = decoded_count + 1
-                except UnicodeDecodeError as e:
-                    if e.__str__() == ("'utf-8' codec can't decode byte 0xf0 "
-                                       "in position 0: invalid continuation "
-                                       "byte"):
-                        invalid_continuation_byte += 1
-                        try:
-                            inval_cont_bytes[c1].append((c2,c3))
-                        except KeyError:
-                            inval_cont_bytes[c1] = [(c2,c3)]
-                    else:
-                        other_exception_count = other_exception_count + 1
-    assert invalid_continuation_byte + \
-           other_exception_count + \
-           decoded_count == total_count
-    assert max(inval_cont_bytes.keys()) == 143
-    assert min(inval_cont_bytes.keys()) == 128
 
-MIN_VALID_FOURBYTE = FourByte(MIN_LEADING, 144, 128, 128)
-MAX_INVALID_FOURBYTE = FourByte(MIN_LEADING, 143, 191, 191)
-
-
-def test_min_valid_fb():
-    with pytest.raises(UnicodeDecodeError):
-        bytes(MAX_INVALID_FOURBYTE).decode()
-    bytes(MIN_VALID_FOURBYTE).decode()
+def test_scan_leading_f1_byte_range():
+    minfb = FourByte(MIN_LEADING+1,
+                     MIN_CONTINUATION,
+                     MIN_CONTINUATION,
+                     MIN_CONTINUATION)
+    maxfb = FourByte(MIN_LEADING+1,
+                     MAX_CONTINUATION,
+                     MAX_CONTINUATION,
+                     MAX_CONTINUATION)
+    total_scans, scan_summary = _scan_range(minfb, maxfb)
