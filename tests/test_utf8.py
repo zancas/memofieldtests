d1999f359fb459a6d6dc1e682d85a79e899812df
Za Wilgustus <zancas@contains.io>
2018-06-20 10:24:24 -0600

    confirm the limits of the leading 240 space

--- tests/test_utf8.py
+++ tests/test_utf8.py
@@ -45,6 +45,9 @@ def test_scan_leading_f0_byte():
                       MIN_CONTINUATION)
     cant_decode_count = 0
     total_count = 0
+    other_exception_count = 0
+    decoded_count = 0
+    undecodeables = {}
     for c1 in range(MIN_CONTINUATION, MAX_CONTINUATION+1):
         for c2 in range(MIN_CONTINUATION, MAX_CONTINUATION+1):
             for c3 in range(MIN_CONTINUATION, MAX_CONTINUATION+1):
@@ -52,13 +55,26 @@ def test_scan_leading_f0_byte():
                 try:
                     temp_fb = FourByte(MIN_LEADING,
                                        c1, c2, c3)
-                    print(temp_fb)
-                    bytes(temp_fb).decode("utf-8")
+                    decoded = bytes(temp_fb).decode("utf-8")
+                    decoded_count = decoded_count + 1
                 except UnicodeDecodeError as e:
                     if e.__str__() == "'utf-8' codec can't decode byte 0xf0 in position 0: invalid continuation byte":
-                       cant_decode_count = cant_decode_count + 1
-    print(cant_decode_count)
-    print("total_count is: %s", cant_decode_count)
+                        cant_decode_count = cant_decode_count + 1
+                        try:
+                            undecodeables[c1].append((c2,c3))
+                        except KeyError:
+                            undecodeables[c1] = [(c2,c3)]
+                    else:
+                        other_exception_count = other_exception_count + 1
+    print("cant_decode_count: ", cant_decode_count)
+    print("total_count is: ", total_count)
+    print("other_exception_count is: ", other_exception_count)
+    print("decoded_count is: ", decoded_count)
+    from pprint import pprint as pp
+    pp(len(undecodeables))
+    pp(undecodeables.keys())
+    assert max(undecodeables.keys()) == 143
+    assert min(undecodeables.keys()) == 128
 
 
 def itest_utf8_bounds():
