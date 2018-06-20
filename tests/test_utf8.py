0ee5e10c67febd95a4f0f1bc98673c3b3adb4bc0
Za Wilgustus <zancas@contains.io>
2018-06-20 12:48:23 -0600

    continuing refactor

--- tests/test_utf8.py
+++ tests/test_utf8.py
@@ -14,13 +14,49 @@ MAX_LEADING = 247
 MIN_LEADING = 240
 
 def _scan_range(minfb, maxfb):
-    return (exceptions, except_count, decoded_count)
+    total_scans = 0
+    scan_summary = {"decoded_fbs": [], "exception_fbs": {}}
+    for l in range(minfb.leading, maxfb.leading + 1):
+        for c1 in range(minfb.continuation1, maxfb.continuation1 + 1):
+            for c2 in range(minfb.continuation2, maxfb.continuation2 + 1):
+                for c3 in range(minfb.continuation3, maxfb.continuation3 + 1):
+                    total_scans += 1
+                    try:
+                        current_fb = FourByte(l, c1, c2, c3)
+                        decoded = bytes(current_fb).decode("utf-8")
+                        scan_summary["decoded_fbs"].append(current_fb)
+                    except UnicodeDecodeError as e:
+                        if e.__str__() in scan_summary["exception_fbs"]:
+                            scan_summary["exception_fbs"][e.__str__()]\
+                                .append(current_fb)
+                        else:
+                            scan_summary["exception_fbs"][e.__str__()] =\
+                                [current_fb]
+    
+    return total_scans, scan_summary
+
+def test_scan_leading_f0_byte_range():
+    minfb = FourByte(MIN_LEADING,
+                     MIN_CONTINUATION,
+                     MIN_CONTINUATION,
+                     MIN_CONTINUATION)
+    maxfb = FourByte(MIN_LEADING,
+                     MAX_CONTINUATION,
+                     MAX_CONTINUATION,
+                     MAX_CONTINUATION)
+    total_scans, scan_summary = _scan_range(minfb, maxfb)
+    EXPECTED_EXCEPTION_MESSAGE = "'utf-8' codec can't decode byte 0xf0 in " +\
+                                 "position 0: invalid continuation byte"
+    EXPECTED_EXCEPTION_MESSAGES = [EXPECTED_EXCEPTION_MESSAGE]
+    OBSERVED_EXCEPTION_MESSAGES =\
+        [x for x in scan_summary["exception_fbs"].keys()]
+    assert total_scans == 2**18
+    assert EXPECTED_EXCEPTION_MESSAGES == OBSERVED_EXCEPTION_MESSAGES
+    undecodedfbs = scan_summary["exception_fbs"][EXPECTED_EXCEPTION_MESSAGE]
+    for x in map(lambda x: x.continuation1, undecodedfbs):
+        pass
 
 def test_scan_leading_f0_byte():
-    low_fb = FourByte(MIN_LEADING,
-                      MIN_CONTINUATION,
-                      MIN_CONTINUATION,
-                      MIN_CONTINUATION)
     invalid_continuation_byte = 0
     total_count = 0
     other_exception_count = 0
@@ -46,7 +82,6 @@ def test_scan_leading_f0_byte():
                             inval_cont_bytes[c1] = [(c2,c3)]
                     else:
                         other_exception_count = other_exception_count + 1
-    assert total_count == 2**18
     assert invalid_continuation_byte + \
            other_exception_count + \
            decoded_count == total_count
