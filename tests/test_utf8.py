d0ff95a3357675682dc24592f422ef40e60651b8
Za Wilgustus <zancas@contains.io>
2018-06-20 10:04:56 -0600

    stash lowercout

--- tests/test_utf8.py
+++ tests/test_utf8.py
@@ -22,6 +22,7 @@ def _search_leading(four_byte):
 def _search_continuation(four_byte):
     return (max_valid, min_invalid)
 
+
 def test_search__leading():
     temp_l = MIN_LEADING - 1
     while temp_l <= MAX_LEADING: 
@@ -36,26 +37,29 @@ def test_search__leading():
             print(e.__str__())
             break
 
-def test_search_f0_invalid_continuation_byte():
+
+def test_scan_leading_f0_byte():
     low_fb = FourByte(MIN_LEADING,
                       MIN_CONTINUATION,
                       MIN_CONTINUATION,
                       MIN_CONTINUATION)
-    count = 0
+    cant_decode_count = 0
+    total_count = 0
     for c1 in range(MIN_CONTINUATION, MAX_CONTINUATION+1):
         for c2 in range(MIN_CONTINUATION, MAX_CONTINUATION+1):
             for c3 in range(MIN_CONTINUATION, MAX_CONTINUATION+1):
-                count = count + 1
+                total_count = total_count + 1
                 try:
                     temp_fb = FourByte(MIN_LEADING,
                                        c1, c2, c3)
                     print(temp_fb)
                     bytes(temp_fb).decode("utf-8")
                 except UnicodeDecodeError as e:
-                    if e.__str__() != "'utf-8' codec can't decode byte 0xf0 in position 0: invalid continuation byte":
-                        print(e.__str__())
-                        break
-    print(count)
+                    if e.__str__() == "'utf-8' codec can't decode byte 0xf0 in position 0: invalid continuation byte":
+                       cant_decode_count = cant_decode_count + 1
+    print(cant_decode_count)
+    print("total_count is: %s", cant_decode_count)
+
 
 def itest_utf8_bounds():
     valid_fb = FourByte(241, 128, 128, 128)
