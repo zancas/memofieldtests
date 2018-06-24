d8072429d28fa4d841e3cb2389fa025bf94a993b
Za Wilgustus <zancas@contains.io>
2018-06-24 17:34:30 -0600

    record result of winZEC v1.1.0 memoless transaction

--- tests/test_zcashcli.py
+++ tests/test_zcashcli.py
@@ -1,43 +1,52 @@
+import json
+import pytest
 import subprocess
 import time
 import uuid
 
-MINORVERSION = 0
-REFCONTAINER = f"zcash-v1.1.{MINORVERSION}-tester:latest"
-UNIQUESUFFIX = uuid.uuid4().urn[9:]
-IMAGETAG = f"zcash-v1.1.{MINORVERSION}-tester:{UNIQUESUFFIX}"
-tagrc = subprocess.call(f"docker tag {REFCONTAINER} {IMAGETAG}".split())
-print(f"IMAGETAG is {IMAGETAG}")
-print(f"tagrc is: {tagrc}")
-so = open("zcashd.out", "w")
-ZCASHDCONTAINER = subprocess.Popen(f"docker run -t {IMAGETAG}".split(),
-                                   stdout=so)
-container = ''
-increment = .00001
-total = 0
-while True:
-    time.sleep(increment)
-    total += increment
-    containers = subprocess.getoutput("docker ps")
-    if IMAGETAG in containers:
-        for line in containers.split('\n'):
-            if IMAGETAG in line:
-                print(line)
-                container = line
-                break
-        break
-print(f"total: {total}")
-containerID = container.split()[0]
-print(containerID)
 
-def test_run_hello_world():
-    print("hello")
-    #print(subprocess.getoutput(f"{ZCASH_CLI} help"))
-    #print(subprocess.getoutput(f"{ZCASH_CLI} z_getnewaddress"))
-    #print(subprocess.getoutput(f"{ZCASH_CLI} z_getoperationstatus"
-    #    " '[\"opid-a377a0b0-3ffd-4c2b-95c8-4aef3b2def8a\"]'"))
-    #print(subprocess.getoutput(f"docker stop {IMAGETAG}"))
-    
+#T_ADDRESS_LOCAL_0 = "tmQD1sgq1jqG9NZiyjeUFRwvLM3CBBnNevw" 
+#Z_ADDRESS_LOCAL_0 = 
 
-    
+@pytest.fixture(scope="module")
+def v1_1_0_containerid(request):
+    MINORVERSION = 0
+    REFCONTAINER = f"zcash-v1.1.{MINORVERSION}-tester:block256675"
+    UNIQUESUFFIX = uuid.uuid4().urn[9:]
+    IMAGETAG = f"zcash-v1.1.{MINORVERSION}-tester:{UNIQUESUFFIX}"
+    tagrc = subprocess.call(f"docker tag {REFCONTAINER} {IMAGETAG}".split())
+    so = open("zcashd.out", "w")
+    ZCASHDCONTAINER = subprocess.Popen(f"docker run -t {IMAGETAG}".split(),
+                                       stdout=so)
+    increment = .00001
+    total = 0
+    while True:
+        time.sleep(increment)
+        total += increment
+        containers = subprocess.getoutput("docker ps")
+        if IMAGETAG in containers:
+            for line in containers.split('\n'):
+                if IMAGETAG in line:
+                    container = line
+                    break
+            break
+    containerID = container.split()[0]
+    def cleaner():
+        #  TODO: Check return codes.
+        stopcode = subprocess.call(f"docker stop {containerID}".split())
+        rmtagcode = subprocess.call(f"docker rmi {IMAGETAG}".split())
+    request.addfinalizer(cleaner)
+    return containerID
 
+def _produce_command_prefix(containerid):
+    Z_CLI_PATH = "/home/zcasher/zcash/src/zcash-cli -rpcwait"
+    DOCKER_PREFIX_TEMPLATE = "docker exec {CONTAINERID}"
+    DOCKER_PREFIX = DOCKER_PREFIX_TEMPLATE.format(CONTAINERID=containerid)
+    return f"{DOCKER_PREFIX} {Z_CLI_PATH}"
+
+def _obtain_TAZ_loaded_taddress(cmd_prefix):
+    #return taddress
+
+def test_zcashd_encoding_of_empty_memo(v1_1_0_containerid):
+    command_prefix = _produce_command_prefix(v1_1_0_containerid)
+    _obtain_TAZ_loaded_taddress(command_prefix)
